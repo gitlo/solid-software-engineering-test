d091201c947382e9411ed0fa820e2b9ea803d879
mcptech <kezman01@gmail.com>
2017-09-20 23:00:29 +0100

    seperate command

--- App/Model/Customer.cs
+++ App/Model/Customer.cs
@@ -20,19 +20,4 @@ namespace App
 
         public Company Company { get; set; }
     }
-
-    public class CustomerAddCommand
-    {
-        public int Id { get; set; }
-
-        public string Firstname { get; set; }
-
-        public string Surname { get; set; }
-
-        public DateTime DateOfBirth { get; set; }
-
-        public string EmailAddress { get; set; }        
-
-        public int CompanyId { get; set; }
-    }
 }
---
+++ App/Model/CustomerAddCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace App
+{
+    public class CustomerAddCommand
+    {
+        public int Id { get; set; }
+
+        public string Firstname { get; set; }
+
+        public string Surname { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string EmailAddress { get; set; }        
+
+        public int CompanyId { get; set; }
+    }
+}
